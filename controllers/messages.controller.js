bec9ab6da1ab6b45e43fd5a45dcef0102863313c
kronosGR <geo.elgeo@gmail.com>
2021-12-22 12:11:13 +0100

    last commit

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -2,7 +2,11 @@ const path = require('path');
 
 function getMessages(req, res) {
   //res.send('Helloo');
-  res.sendFile(path.join(__dirname, '..', 'public', 'images', 'img.png'));
+  //res.sendFile(path.join(__dirname, '..', 'public', 'images', 'img.png'));
+  res.render('message', {
+    title: 'Message to my friends',
+    friend: 'Kronos'
+  });
 }
 
 function postMessage(req, res) {
